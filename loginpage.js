20d10fd1132d856830f5395130fb98dc357435d9
runnung-wrench <135842277+runnung-wrench@users.noreply.github.com>
2023-06-07 21:25:49 +0800

    Add files via upload

---
+++ loginpage.js
@@ -0,0 +1,13 @@
+var password = "sanchez131" ;
+function passcheck(){
+
+    if(document.getElementById('pass1').value != password){
+        alert('You are wrong, a-hole');
+        return false
+    }
+}
+
+if(document.getElementById('pass').value ==password) {
+    alert("just get in");
+}
+
